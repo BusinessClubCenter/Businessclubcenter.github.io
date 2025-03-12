eaac4b52bce7f9565f58396f5e141f695aada032
Business_club <azzonov2526@gmail.com>
2025-03-12 17:22:39 +0700

    Add files via upload

--- redirect.js
+++ redirect.js
@@ -6,7 +6,7 @@ const redirectMap = {
     'timeweb': 'https://craftum.com/?i=122719',
     'taxcom': 'https://taxcom.ru/partner-form/?refLinkId=74349',
     'hosting': 'https://timeweb.com/ru/?i=122719',
-    'sprinthost': 'https://sprinthost.ru/s42646/?bid=193' // Добавлен Sprinthost
+    'sprinthost': 'https://sprinthost.ru/s42646/?bid=193', // Добавлен Sprinthost       
 };
 // Устанавливаем редирект
 const redirectUrl = redirectMap[ref] || '#';
